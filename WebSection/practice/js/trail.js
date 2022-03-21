9b52dcdd1ea670f517d37c0a178cd5aa3be69174
WEN SHUFA <SHUFA17@GMAIL.COM>
2022-03-21 21:37:56 +0800

    add navigation bar practice and js code for week 3 day 1

---
+++ WebSection/practice/js/trail.js
@@ -0,0 +1,18 @@
+document.write("Hello, this is my JS Code from external file<br>");
+document.write(2022);
+document.write("<br>");
+document.write("The value of 4+4 is " + (4 + 4));
+document.write("<br>");
+document.write("2" + "4");
+document.write("<br>");
+document.write(2,4); // , used to append
+document.write("<br><br>");
+
+window.document.write("Hello, this is my JS Code from external file<br>");
+window.document.write(2022);
+window.document.write("<br>");
+window.document.write("The value of 4+4 is " + (4 + 4));
+window.document.write("<br>");
+window.document.write("2" + "4");
+window.document.write("<br>");
+window.document.write(2,4,"<br>"); // , used to append
