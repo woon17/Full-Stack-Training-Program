8df5ccbb6cf19a134d6141c01097d705e8fedd46
WEN SHUFA <SHUFA17@GMAIL.COM>
2022-03-30 19:38:42 +0800

    add week 4 day 3 web section code
    callbacks, promise, js form validation

---
+++ WebSection/practice/js learning/learn validation/form.js
@@ -0,0 +1,25 @@
+function check() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var mobile = document.getElementById("mobile").value;
+    var password = document.getElementById("password").value;
+    // var comfirmPassword = document.getElementById("comfirmPassword");
+    // console.log("name: " + isLetter(name));
+    console.log(isLetter(name));
+    // let name_exp = new RegExp('[A-Za-z]');
+    // alert(name_exp.test(name));
+    // if (name_exp.test(name)) {
+    //     alert("valid");
+    // } else {
+    //     alert("Name files is not valid");
+    // }
+
+
+    // let email_exp = new RegExp('[a-z0-9.]+@[a-z]+\.[a-z]{2,3}');
+    // alert(email_exp.test(email));
+    // if (email_exp.test(email)) {
+    //     alert("valid");
+    // } else {
+    //     alert("email is not valid");
+    // }
+}
