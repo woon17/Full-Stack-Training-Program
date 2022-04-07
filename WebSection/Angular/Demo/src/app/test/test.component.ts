0c683a81552645d809fac04bf299ebc1227a390b
WEN SHUFA <SHUFA17@GMAIL.COM>
2022-04-07 17:03:33 +0800

    child component send message by event to parent component
    using @Inpput @output

--- WebSection/Angular/Demo/src/app/test/test.component.ts
+++ WebSection/Angular/Demo/src/app/test/test.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
+// import { EventEmitter } from 'stream';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-test',
@@ -6,15 +8,22 @@ import { Component, OnInit, Input } from '@angular/core';
   // templateUrl: './test.componentIfElse.html',
   // templateUrl: './test.componentSwitchCases.html',
   // templateUrl: './test.componentFor.html',
-  templateUrl: './test.componentFromParentComponent.html',
+  // templateUrl: './test.componentFromParentComponent.html',
+  templateUrl: './test.componentToParentComponent.html',
   styleUrls: ['./test.component.css'],
 })
 export class TestComponent implements OnInit {
   public disp = true; //used for if else test
   public day = 'mon';
   public participants = ['wen', 'shu', 'fa', 'wu'];
-  @Input() public dataFromParent = "";
+  // @Input() public dataFromParent = '';
+  @Input('dataFromParent') public test = '';
+  @Output() public childEvent = new EventEmitter();
   constructor() {}
 
   ngOnInit(): void {}
+
+  sendData() {
+    this.childEvent.emit("Hello parent");
+  }
 }
