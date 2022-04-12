046f80ec6aba84785907226f3425dcda0b0eba9a
WEN SHUFA <SHUFA17@GMAIL.COM>
2022-04-12 16:30:39 +0800

    prefilled value for form and one way binding

--- WebSection/Angular/AngularForms/src/app/app.component.ts
+++ WebSection/Angular/AngularForms/src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'AngularForms';
-  cities = ['Singapore','India', 'China', 'Malaysia']
+  cities = ['Singapore', 'India', 'China', 'Malaysia'];
+  userData = new User(
+    'shufa',
+    'shufa17@gmail.com',
+    12345678,
+    'Singapore',
+    'male',
+    true
+  );
 }
