f29c3fc0c4e5763ffe8867c27b0e8aeb9afa7696
WEN SHUFA <SHUFA17@GMAIL.COM>
2022-04-13 17:38:30 +0800

    with IOC, but no setter injection

--- MySpringSetterInjection/src/com/dxc/springSetterInjection/MainClass.java
+++ MySpringSetterInjection/src/com/dxc/springSetterInjection/MainClass.java
@@ -1,13 +1,25 @@
 package com.dxc.springSetterInjection;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Employee emp = new Employee();
+		
+		// without IOC
+//		Employee emp = new Employee();
+//		emp.setEmpId(1);
+//		emp.setEmpName("shufa");
+//		System.out.println(emp);
+		//with IOC
+		ApplicationContext context = new ClassPathXmlApplicationContext("appContext.xml");
+		Employee emp = (Employee) context.getBean("emp");
 		emp.setEmpId(1);
 		emp.setEmpName("shufa");
 		System.out.println(emp);
+		
 	}
 
 }
