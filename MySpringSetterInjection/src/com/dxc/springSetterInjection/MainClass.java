ffe51f846133743418fb226c69f95d7e4c710dee
WEN SHUFA <SHUFA17@GMAIL.COM>
2022-04-13 17:31:24 +0800

    without IOC

---
+++ MySpringSetterInjection/src/com/dxc/springSetterInjection/MainClass.java
@@ -0,0 +1,13 @@
+package com.dxc.springSetterInjection;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee emp = new Employee();
+		emp.setEmpId(1);
+		emp.setEmpName("shufa");
+		System.out.println(emp);
+	}
+
+}
