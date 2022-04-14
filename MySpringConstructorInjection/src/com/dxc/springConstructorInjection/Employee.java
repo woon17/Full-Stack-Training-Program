506f400d0b144f62ce955f76fb10088a5a986615
WEN SHUFA <SHUFA17@GMAIL.COM>
2022-04-14 14:48:05 +0800

    constructor injection with prototype scope
    <constructor-arg>

---
+++ MySpringConstructorInjection/src/com/dxc/springConstructorInjection/Employee.java
@@ -0,0 +1,39 @@
+package com.dxc.springConstructorInjection;
+
+public class Employee {
+	private int empId;
+	private String empName;
+
+	public Employee(int empId, String empName) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+	}
+	
+	public Employee() {
+		
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	@Override
+	public String toString() {
+		return this.getClass().getName() + "@" + 
+		           Integer.toHexString(System.identityHashCode(this)) + " [empId=" + empId + ", empName=" + empName + "]";
+	}
+
+}
