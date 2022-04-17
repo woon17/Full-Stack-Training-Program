8a3903800a8d0d38bc8f6903af517517d6ddd8f0
WEN SHUFA <SHUFA17@GMAIL.COM>
2022-04-18 00:06:35 +0800

    ssetter injection  with prototype scope

---
+++ MySpringBeanLifeCycle/src/com/dxc/springSetterInjection/Employee.java
@@ -0,0 +1,38 @@
+package com.dxc.springSetterInjection;
+
+public class Employee {
+	private int empId;
+	private String empName;
+
+	public Employee(int empId, String empName) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+	}
+	
+	public Employee() {
+		
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+//	@Override
+//	public String toString() {
+//		return "Employee [empId=" + empId + ", empName=" + empName + "]";
+//	}
+
+}
---
+++ MySpringBeanLifeCycle/src/com/dxc/springSetterInjection/MainClass.java
@@ -0,0 +1,35 @@
+package com.dxc.springSetterInjection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// without IOC
+//		Employee emp = new Employee();
+//		emp.setEmpId(1);
+//		emp.setEmpName("shufa");
+//		System.out.println(emp);
+		//with IOC
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("appContext.xml");
+		Employee emp = (Employee) context.getBean("emp");
+		// without setter injection
+//		emp.setEmpId(1);
+//		emp.setEmpName("wen");
+		
+		Employee emp2 = (Employee) context.getBean("emp");
+		Employee emp3 = (Employee) context.getBean("emp");
+//		emp2.setEmpName("test");
+		System.out.println(emp);
+		System.out.println(emp2);
+		System.out.println(emp3);
+		
+		
+		
+	}
+
+}
