49860a5c56544c919710049472692abf306925d1
WEN SHUFA <SHUFA17@GMAIL.COM>
2022-04-19 14:05:21 +0800

    one to many relationship
    bidirectional

---
+++ one-to-many-mapping-insertion/src/Course.java
@@ -0,0 +1,68 @@
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "COURSE")
+public class Course {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "title")
+	private String title;
+
+	// many courses to one student
+	// do not choose CascadeType.REMOVE and CascadeType.ALL
+	@ManyToOne(cascade = { CascadeType.MERGE, CascadeType.DETACH, CascadeType.PERSIST, CascadeType.REFRESH })
+	//f_key 
+	@JoinColumn(name = "studentId") // studentDetailId from student table
+	private Student student;
+
+	public Course() {
+
+	}
+
+	public Course(String title) {
+		super();
+		this.title = title;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", title=" + title + ", student=" + student + "]";
+	}
+
+
+}
