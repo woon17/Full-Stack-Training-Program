e5ff8e2bc736ad88973f1df74ff2dceed371d9ef
WEN SHUFA <SHUFA17@GMAIL.COM>
2022-04-19 15:02:01 +0800

    add form page

---
+++ Spring-MVC-Demo/WebContent/WEB-INF/view/show-form.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Please Enter Details</title>
+</head>
+<body>
+	<form action="processForm" method="GET">
+		<input type="text" name="name" placeholder="Enter your name">
+		<input type="text" name="role" placeholder="Enter your role">
+		<input type="submit" value="submit details">
+	</form>
+
+</body>
+</html>
--- Spring-MVC-Demo/src/com/dxc/springdemo/HomeController.java
+++ Spring-MVC-Demo/src/com/dxc/springdemo/HomeController.java
@@ -6,15 +6,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-	@RequestMapping("/")//execute when there is request from "/"
+	@RequestMapping("/") // execute when there is request from "/"
 	public String showHomePage() {
 		// it will append prefix and suffix as we set up in step 5 in xml file
 		return "main-menu";
 	}
-	
-	@RequestMapping("/showTrail")//execute when there is request from "/showTrail"
+
+	@RequestMapping("/demo") // execute when there is request from "/demo"
 	public String showTrail() {
 		// it will append prefix and suffix as we set up in step 5 in xml file
 		return "show-trial";
 	}
+
+	@RequestMapping("/showForm") // execute when there is request from "/showForm"
+	public String showForm() {
+		// it will append prefix and suffix as we set up in step 5 in xml file
+		return "show-form";
+	}
+
+	@RequestMapping("/processForm") // execute when there is request from "/processForm"
+	public String processForm() {
+		// it will append prefix and suffix as we set up in step 5 in xml file
+		return "hello-world";
+	}
+
 }
